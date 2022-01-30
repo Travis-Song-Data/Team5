b4dd6f5a11e750faa99ac00087977515ad0a0f40
Na Rah Kwak <kwa18001@byui.edu>
2022-01-29 19:34:16 -0700

    tiny fix

--- HiLo/Hilo-Xing/player.py
+++ HiLo/Hilo-Xing/player.py
@@ -1,8 +1,9 @@
 from card import Card
 
+
 class Player:
     """A person who play the game. 
-    
+
     The responsibility of a Player is to control the sequence of play.
 
     Attributes:
@@ -11,29 +12,28 @@ class Player:
         points(int): The points of one round play.
         answer(str): A value to decide the next card is higher than the first card or not
     """
-    
+
     def __init__(self):
         """Constructs a new Player.
-        
+
         Args:
             self (Player): an instance of Player.
         """
         self.keep_playing = True
-        self.total_scores = 400
+        self.total_scores = 300
         self.points = 0
         self.answer = ''
 
-
     def start(self):
         """Start the game by runing the main game loop.
-        
+
         Args:
             self (Player): an instance of Player.
         """
         card = Card()
         card.draw_card()
         while self.keep_playing:
-            card_one = card.drawn_card 
+            card_one = card.drawn_card
             card.draw_card()
             card_two = card.drawn_card
             if card_two > card_one:
@@ -49,22 +49,21 @@ class Player:
             self.lose_game()
             self.playing_or_not()
 
-
     def playing_or_not(self):
         """Start the game by runing the main game loop.
-        
+
         Args:
             self (Player): an instance of Player.
         """
         if not self.keep_playing:
             return
-            
+
         playing_or_not = input('Play again? [y/n]')
         self.keep_playing = (playing_or_not == 'y')
 
     def player_guess(self):
         '''Ask the player to guess the second card is higher or lower than the first card.
-        
+
         Args:
             self(Scores): An instance of Socres
         '''
@@ -76,7 +75,7 @@ class Player:
 
     def display_score(self):
         """Display the total socres to player.
-        
+
         Args:
             self (Player): an instance of Player.
         """
@@ -85,12 +84,10 @@ class Player:
 
     def lose_game(self):
         """Check if the total scores is beyond 0, if it is, then the game is over.
-        
+
         Args:
             self (Player): an instance of Player.
         """
         if self.total_scores <= 0:
             print('You lose, game over.')
             self.keep_playing = False
-
-    
